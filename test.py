bb32df0a467f26c578dfac300a88c8230a60f402
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2024-11-16 15:29:30 +0000

    Update PyAppDevKit.

---
+++ test.py
@@ -0,0 +1,4 @@
+from LibFunc.pyappdevkit import *
+
+program_info("NAME:","TEST [PROGRAM_INFO]","","","","","","","","","","","","","","","","","","")
+library_info("NAME:","TEST [LIBRARY_INFO]","","","","","","","","","","","","","","","","","","")
