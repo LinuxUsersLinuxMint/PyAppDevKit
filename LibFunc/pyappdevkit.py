4c033fe6d17bd2ef70389c9e540391fd3a0e2a67
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-01-01 19:03:19 +0300

    Update PyAppDevKit.

--- LibFunc/pyappdevkit.py
+++ LibFunc/pyappdevkit.py
@@ -5,7 +5,11 @@ PyAppDevKit All Rights Reserved under the GPL(General Public License).
 Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/PyAppDevKit
 A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/PyAppDevKit"""
 
-import time
+def time(number):
+    while number > 0:
+        number -= 1
+        for _ in range(100000000):
+            pass
 
 def error_msg(error_dialog):
     print(error_dialog)
@@ -13,11 +17,13 @@ def error_msg(error_dialog):
 def exit_program_dialog_time(exit_dialog_msg,userTime):
     print(exit_dialog_msg)
     userTime = int(userTime)
-    time.sleep(userTime)
+    time(userTime)
+    exit()
 
 def exit_program_time(userTime):
     userTime = int(userTime)
-    time.sleep(userTime)
+    time(userTime)
+    exit()
 
 def exit_program_dialog(exit_dialog_msg):
     print(exit_dialog_msg)
